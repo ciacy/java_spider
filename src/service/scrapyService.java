1e816c4a407a6a214e6f883518a2443a73fddde1
saic <635525852@qq.com>
2017-04-24 17:58:25 +0800

    11

--- src/service/scrapyService.java
+++ src/service/scrapyService.java
@@ -1,23 +1,44 @@
+import java.net.URL;
+
 
 public class scrapyService implements scrapyImpl{
+	
+	
+
 	
 	
 	private  void  simulateUser(){};
 	
 	
-	
+    this(protocol, host, port, file, null);
+    
+    
+    // 方法一 
+    URL url = new URL("http://www.sina.com.cn");
+    URLConnection urlcon = url.openConnection();
+    InputStream is = urlcon.getInputStream();
+   
+     // 方法二
+    URL url = new URL("http://www.yhfund.com.cn");
+    HttpURLConnection urlcon = (HttpURLConnection)url.openConnection();
+    InputStream is = urlcon.getInputStream();
+   
+    //方法三
+    URL url = new URL("http://www.yhfund.com.cn");
+    InputStream is = url.openStream();
 	
 	
 	private  void   getResource(){
 		
 		
+		URL url=new Url();
+		
+		
+	
 		
 		
 		
-		
-		
-		
-		
+	
 		
 		
 	};
